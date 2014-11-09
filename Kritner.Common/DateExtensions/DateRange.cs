78b27c3f5f2cde7e2835678707eba6a9390d9b47
Kritner <Kritner@gmail.com>
2014-11-09 18:08:06 -0500

    Added ToString and Update dates functions/tests

--- Kritner.Common/DateExtensions/DateRange.cs
+++ Kritner.Common/DateExtensions/DateRange.cs
@@ -12,6 +12,9 @@ namespace Kritner.Common.DateExtensions
 
         public DateTime StartDate { get; private set; }
         public DateTime EndDate { get; private set; }
+        public TimeSpan DateRangeSpan { get { return EndDate - StartDate; } }
+
+        private const string _DateFormat = "dd/MM/yyyy";
 
         /// <summary>
         /// Constructor - Create a DateRange object
@@ -25,6 +28,27 @@ namespace Kritner.Common.DateExtensions
 
             this.StartDate = startDate;
             this.EndDate = endDate;
+        }
+
+        /// <summary>
+        /// Returns a new DateRange object with the specified startDate and endDate 
+        /// (rather than changing the current values, just create and return a new object)
+        /// </summary>
+        /// <param name="startDate">Start date of the date range</param>
+        /// <param name="endDate">End date of the date range</param>
+        /// <returns>DateRange</returns>
+        public DateRange GetNewDateRange(DateTime startDate, DateTime endDate)
+        {
+            return new DateRange(startDate, endDate);
+        }
+
+        /// <summary>
+        /// Display the string representation of the DateRange
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("{0} - {1}", this.StartDate.ToString(_DateFormat), EndDate.ToString(_DateFormat));
         }
 
     }
