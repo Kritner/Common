5c0c7b913ef6f5064b39f400c07904b72d8b677d
Russ Hammett <Kritner@gmail.com>
2016-12-24 15:09:30 -0500

    CodeWars

---
+++ Kritner.Common/CodeWars/AreTheySame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kritner.Common.CodeWars
+{
+    public class AreTheySame
+    {
+        public static bool comp(int[] a, int[] b)
+        {
+            if (a == null || b == null || a.Length != b.Length)
+                return false;
+
+            int[] aSquare = a.Select(s => s * s).ToArray();
+            Array.Sort(aSquare);
+            Array.Sort(b);
+
+            for (int i = 0; i < aSquare.Length; i++)
+                if (aSquare[i] != b[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ Kritner.Common/CodeWars/FindShort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kritner.Common.CodeWars
+{
+    public class FindShort
+    {
+        public static int FindShortestWordLength(string input)
+        {
+            return input
+                .Split(' ')
+                .Select(s => s.Length)
+                .Min();
+        }
+    }
+}
