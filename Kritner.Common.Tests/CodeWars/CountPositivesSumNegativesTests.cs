f6ab9479a383c8f1895641cfa76e229b48803a93
Russ Hammett <Kritner@gmail.com>
2017-01-02 08:50:57 -0500

    CountPositivesSumNegatives https://www.codewars.com/kata/576bb71bbbcf0951d5000044

---
+++ Kritner.Common.Tests/CodeWars/CountPositivesSumNegativesTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using Kritner.Common.CodeWars;
+
+namespace Kritner.Common.Tests.CodeWars
+{
+    [TestFixture]
+    public class CountPositivesSumNegativesTests
+    {
+        [Test]
+        public void CountPositivesSumNegatives_BasicTest()
+        {
+            int[] expectedResult = new int[] { 10, -65 };
+
+            Assert
+                .AreEqual(
+                    expectedResult, 
+                    CountPositivesSumNegatives.DoStuff(
+                        new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 }
+                    )
+                );
+        }
+
+        [Test]
+        public void CountPositivesSumNegatives_InputWithZeroes()
+        {
+            int[] expectedResult = new int[] { 8, -50 };
+
+            Assert
+                .AreEqual(
+                    expectedResult, 
+                    CountPositivesSumNegatives.DoStuff(
+                        new[] { 0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14 }
+                    )
+                );
+        }
+    }
+}
