3b067ffe713e89d7a659223cf43fc49933ce891c
Russ Hammett <Kritner@gmail.com>
2017-07-14 15:57:43 -0400

    DecendingOrder

---
+++ Kritner.Common.Tests/CodeWars/DescendingOrderTests.cs
@@ -0,0 +1,34 @@
+﻿using Kritner.Common.CodeWars;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kritner.Common.Tests.CodeWars
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test0()
+        {
+            Assert.AreEqual(0, DescendingOrderKata.DescendingOrder(0));
+        }
+
+        [Test]
+        public void NegativeThrows()
+        {
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => DescendingOrderKata.DescendingOrder(-1));
+        }
+
+        [Test]
+        [TestCase(12345, 54321)]
+        [TestCase(11112, 21111)]
+        public void Test1(int provided, int expectation)
+        {
+            Assert.AreEqual(expectation, DescendingOrderKata.DescendingOrder(provided));
+        }
+    }
+}
---
+++ Kritner.Common/CodeWars/DescendingOrderKata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Kritner.Common.CodeWars
+{
+    public class DescendingOrderKata
+    {
+        /// <summary>
+        /// Your task is to make a function that can take any non-negative integer as a argument and return it with it's digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+        /// </summary>
+        /// <example>
+        ///         Examples:
+        ///         Input: 21445 Output: 54421
+        ///         Input: 145263 Output: 654321
+        ///         Input: 1254859723 Output: 9875543221
+        /// </example>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int DescendingOrder(int num)
+        {
+            if (num < 0)
+                throw new ArgumentOutOfRangeException();
+
+            var numberDigits = num.ToString().ToCharArray();
+
+            var newString = new string (numberDigits.OrderByDescending(ob => ob).AsEnumerable().ToArray());
+
+            int result = 0;
+            int.TryParse(newString, out result);
+
+            return result;
+        }
+    }
+}
